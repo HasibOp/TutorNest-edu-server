986d66f8db10ddf5ecd7482d0694bc9adda07396
HasibOp <adnanope24@gmail.com>
2026-08-16 14:49:51 +0600

    initialize backend server structure

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const app = require('./app');
+const port = process.env.PORT || 5000;
+
+async function startServer() {
+  try {
+    app.listen(port, () => {
+      console.log(`TutorNest-edu Server is running : ${port}`);
+    });
+  } catch (err) {
+    console.log("Failed to start server:", err);
+  }
+}
+
+startServer();
